a413312e1178586189e8a2ae2e392a176dd4cf07
Subham Sharma <subham.sharma221296@gmail.com>
2026-04-07 19:23:58 +0530

    added projects and fixed some contents

--- src/data/icons.js
+++ src/data/icons.js
@@ -1,97 +1,126 @@
-import {
-  icon1,
-  icon2,
-  icon3,
-  icon4,
-  icon5,
-  icon6,
-  icon7,
-  icon8,
-  icon9,
-  icon10,
-  icon11,
-  icon13,
-  icon14,
-  icon15,
-  icon16
-} from "../images/tools";
+import StackIcon from "tech-stack-icons";
 
 const iconData = [
   {
     iconID: "1",
-    icon: icon3,
+    icon: <StackIcon name="react" />,
     name: "React",
   },
   {
     iconID: "2",
-    icon: icon4,
+    icon: <StackIcon name="nodejs" />,
     name: "NodeJS",
   },
   {
     iconID: "3",
-    icon: icon7,
-    name: "Java",
-  },
-  {
-    iconID: "4",
-    icon: icon8,
+    icon: <StackIcon name="js" />,
     name: "JavaScript",
   },
   {
+    iconID: "4",
+    icon: <StackIcon name="java" />,
+    name: "Java",
+  },
+  {
     iconID: "5",
-    icon: icon1,
+    icon: <StackIcon name="html5" />,
     name: "HTML",
   },
   {
     iconID: "6",
-    icon: icon2,
+    icon: <StackIcon name="css3" />,
     name: "CSS",
   },
   {
     iconID: "7",
-    icon: icon5,
-    name: "VS Code",
+    icon: <StackIcon name="spring" />,
+    name: "Spring Boot",
   },
   {
     iconID: "8",
-    icon: icon6,
-    name: "MongoDB",
+    icon: <StackIcon name="typescript" />,
+    name: "TypeScript",
   },
   {
     iconID: "9",
-    icon: icon9,
-    name: "Amazon Web Services",
+    icon: <StackIcon name="nextjs2" />,
+    name: "Next.js",
   },
   {
     iconID: "10",
-    icon: icon10,
-    name: "Google Cloud Platform",
+    icon: <StackIcon name="aws" variant="dark"/>,
+    name: "Amazon Web Services",
   },
   {
     iconID: "11",
-    icon: icon11,
-    name: "GIT",
+    icon: <StackIcon name="gcloud" />,
+    name: "Google Cloud",
   },
   {
     iconID: "12",
-    icon: icon16,
-    name: "MySQL",
+    icon: <StackIcon name="firebase" />,
+    name: "Firebase",
   },
   {
     iconID: "13",
-    icon: icon13,
-    name: "Postman",
+    icon: <StackIcon name="mysql" />,
+    name: "MySQL",
   },
   {
     iconID: "14",
-    icon: icon14,
-    name: "Google Analytics",
+    icon: <StackIcon name="mongodb" />,
+    name: "MongoDB",
   },
   {
     iconID: "15",
-    icon: icon15,
+    icon: <StackIcon name="docker" />,
+    name: "Docker",
+  },
+  {
+    iconID: "16",
+    icon: <StackIcon name="vscode" />,
+    name: "VS Code",
+  },
+  {
+    iconID: "17",
+    icon: <StackIcon name="cursor" variant="dark" />,
+    name: "Cursor",
+  },
+  {
+    iconID: "18",
+    icon: <StackIcon name="postman" />,
+    name: "Postman",
+  },
+  {
+    iconID: "19",
+    icon: <StackIcon name="netlify"/>,
     name: "Netlify",
   },
+  {
+    iconID: "20",
+    icon: <StackIcon name="vercel" variant="dark"/>,
+    name: "Vercel",
+  },
+  {
+    iconID: "20",
+    icon: <StackIcon name="tailwindcss" />,
+    name: "Tailwind CSS",
+  },
+  {
+    iconID: "21",
+    icon: <StackIcon name="kotlin" />,
+    name: "Kotlin",
+  },
+  {
+    iconID: "22",
+    icon: <StackIcon name="git" />,
+    name: "Git",
+  },
+  {
+    iconID: "21",
+    icon: <StackIcon name="jira" />,
+    name: "Atlassian JIRA",
+  }
 ];
 
 export {iconData};
